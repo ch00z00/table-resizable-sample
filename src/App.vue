89a2df3f5e7cca96f760632beb24e279ff582e3c
ch00z00 <choozoo10@gmail.com>
2025-02-15 05:33:16 +0900

    Implemented table resizable directive

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,77 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <div class="container">
+      <div>Default</div>
+      <table border="1" class="table" v-table-resizable>
+        <thead>
+          <tr>
+            <th width="50%">name</th>
+            <th width="25%">age</th>
+            <th width="25%">gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>20</td>
+            <td>male</td>
+          </tr>
+          <tr>
+            <td>Emma</td>
+            <td>18</td>
+            <td>female</td>
+          </tr>
+          <tr>
+            <td>Peter</td>
+            <td>21</td>
+            <td>male</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="container">
+      <div>Resize on thead</div>
+      <table border="1" class="table">
+        <thead v-table-resizable>
+          <tr>
+            <th width="50%">name</th>
+            <th width="25%">age</th>
+            <th width="25%">gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>20</td>
+            <td>male</td>
+          </tr>
+          <tr>
+            <td>Emma</td>
+            <td>18</td>
+            <td>female</td>
+          </tr>
+          <tr>
+            <td>Peter</td>
+            <td>21</td>
+            <td>male</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 400px;
+  margin-bottom: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.table {
+  width: 100%;
+  border-collapse: collapse;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import TableResizable from './directive/table-resizable.ts';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(TableResizable);
+app.mount('#app');
